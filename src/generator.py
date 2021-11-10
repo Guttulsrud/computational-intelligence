da6b15413bfc4f4365a7d09447dc1f8325a3ac16
arnaunaval-volur <arnau.naval@volur.no>
2021-11-10 14:05:05 +0100

    added flag for augmentation

--- src/generator.py
+++ src/generator.py
@@ -22,7 +22,7 @@ def get_data_generators(config: dict):
     test_data = temp.sample(frac=config['test_split'])
     train_data = temp.drop(test_data.index)
 
-    train_data_generator = Generator(train_data, config)
+    train_data_generator = Generator(train_data, config, augmentation=True)
     validation_data_generator = Generator(validation_data, config)
     test_data_generator = Generator(test_data, config)
 
@@ -32,16 +32,17 @@ def get_data_generators(config: dict):
 def get_data_from_labels(labels: list, max_img_class: int) -> pd.DataFrame:
     df = pd.read_csv(lookup_table)
     df = df[df['label'].isin(labels)]
-    df = df.groupby(['label','label_name']).head(max_img_class)
+    df = df.groupby(['label', 'label_name']).head(max_img_class)
     return df
 
 
 class Generator(tf.keras.utils.Sequence):
-    def __init__(self, df, config=None):
+    def __init__(self, df, config=None, augmentation=False):
         self.config = config
         self.df = df
         self.data_length = len(self.df)
         self.batch_size = config['batch_size']
+        self.augmentation = augmentation
         self.__set_agumentation()
 
     def on_epoch_start(self):
@@ -80,20 +81,22 @@ class Generator(tf.keras.utils.Sequence):
     def __augment_image(self, image, n_filters=-1):
         image = cv2.resize(image, (150, 150), interpolation=cv2.INTER_AREA)
 
-        if n_filters == -1:
-            num_filters = np.random.randint(0, len(self.augmenters))
-        else:
-            num_filters = n_filters
+        if self.augmentation:
 
-        rng = default_rng()
-        filter_index = rng.choice(
-            len(self.augmenters), size=num_filters, replace=False)
+            if n_filters == -1:
+                num_filters = np.random.randint(0, len(self.augmenters))
+            else:
+                num_filters = n_filters
 
-        image = image.astype(np.uint8)
+            rng = default_rng()
+            filter_index = rng.choice(
+                len(self.augmenters), size=num_filters, replace=False)
 
-        for index in filter_index:
-            filter = self.augmenters[index]
-            image = filter(image=image)
+            image = image.astype(np.uint8)
+
+            for index in filter_index:
+                filter = self.augmenters[index]
+                image = filter(image=image)
 
         return image
 
