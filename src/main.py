2553267046d425a26e749d9cc09e2bc1546087ca
arnaunaval-volur <arnau.naval@volur.no>
2021-11-24 10:11:57 +0100

    Revert "merge"
    This reverts commit 40cc9a31ee956e521c44982ba1d27e6eca05241c, reversing
    changes made to 142ba3a3dfd1e3ae987705ba6214b1ac6bdfc8b7.

--- src/main.py
+++ src/main.py
@@ -14,24 +14,21 @@ train_generator, validation_generator, test_generator = get_data_generators(conf
 controller = Controller(config, test_generator)
 
 for count in range(100):
-    try:
-        hyper_parameters = controller.get_random_hyper_parameters()
-        print(f'Running model number {count} \n'
-              f'with hyper parameters: {hyper_parameters}')
-        model = create_model(config=config, hyper_parameters=hyper_parameters)
+    hyper_parameters = controller.get_random_hyper_parameters()
+    print(f'Running model number {count} \n'
+          f'with hyper parameters: {hyper_parameters}')
+    model = create_model(config=config, hyper_parameters=hyper_parameters)
 
-        run_dir = f'{datetime.now().strftime("%Y%m%d-%H%M%S")}'
-        callbacks = init_callbacks(test_generator, config, hyper_parameters, f'logs/{datetime.now().strftime("%Y%m%d-%H%M%S")}')
+    run_dir = f'{datetime.now().strftime("%Y%m%d-%H%M%S")}'
+    callbacks = init_callbacks(test_generator, config, hyper_parameters, f'logs/{datetime.now().strftime("%Y%m%d-%H%M%S")}')
 
-        model.fit(
-            train_generator,
-            batch_size=config['batch_size'],
-            validation_data=validation_generator,
-            epochs=config['number_of_epochs'],
-            callbacks=callbacks
-        )
-        _, accuracy = model.evaluate(test_generator)
+    model.fit(
+        train_generator,
+        batch_size=config['batch_size'],
+        validation_data=validation_generator,
+        epochs=config['number_of_epochs'],
+        callbacks=callbacks
+    )
+    _, accuracy = model.evaluate(test_generator)
 
-        controller.update_hparams(run_dir, hyper_parameters, accuracy)
-    except:
-        pass
+    controller.update_hparams(run_dir, hyper_parameters, accuracy)
